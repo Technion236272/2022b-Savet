ec612d4bd45966fc796a7cedb2b2903a66d470f5
amjad <amjad_aslan@hotmail.com>
2022-05-16 16:36:46 +0300

    commit #19920

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+// @dart=2.9
+
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'auth/login_page.dart';
+import 'auth/auth_repoitory.dart';
+
+import 'homepage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(ChangeNotifierProvider(
+      create: (context) => AuthRepository.instance(), child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          return MaterialApp(
+            title: 'Savet',
+            theme: ThemeData(
+              primarySwatch: Colors.deepOrange,
+            ),
+            home: Splash2(),
+            debugShowCheckedModeBanner: false,
+          );
+        });
+  }
+}
+
+class Splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrange,
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.3),
+      child: SplashScreen(
+        seconds: 3,
+        useLoader: true,
+        navigateAfterSeconds:
+            AuthRepository.instance().isAuthenticated ? homepage() : Login(),
+        title: new Text(
+          '',
+          textScaleFactor: 2,
+          style: TextStyle(color: Colors.white),
+        ),
+        image: Image.asset("assets/image/splash.png"),
+        backgroundColor: Colors.deepOrange,
+        photoSize: 150.0,
+        loaderColor: Colors.white,
+      ),
+    );
+  }
+}
